3acd8277ef6eaee1cf4120b2b4a9040ff8a216d7
Val <82764150+hopkee@users.noreply.github.com>
2022-04-04 22:13:29 +0300

    First commit

---
+++ ColorAppSwiftUI/ColorAppSwiftUI/ColorAppSwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  ColorAppSwiftUIApp.swift
+//  ColorAppSwiftUI
+//
+//  Created by Valya on 4.04.22.
+//
+
+import SwiftUI
+
+@main
+struct ColorAppSwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
